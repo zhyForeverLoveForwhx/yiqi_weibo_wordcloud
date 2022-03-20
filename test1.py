0a42bbb500491bdeccea8437246c4975f455e10f
zhy <1654049765@qq.com>
2022-03-20 19:12:17 +0800

    try the Histogram

--- test1.py
+++ test1.py
@@ -1,32 +1,88 @@
-import csv
+#柱状图的试验
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-#header      = [id,bid,user_id,用户昵称,微博正文,头条文章url,发布位置,艾特用户,话题,转发数,评论数,点赞数,发布时间,发布工具,微博图片url,微博视频url,retweet_id]
-File_Path   = "D:\python_py\weibo_spider\weibo-search\结果文件\%23疫情%23"
-Filename    = "%23疫情%23"
-CSV_Path    = File_Path+"\\"+Filename+".csv"
-Result_Path = Filename + "_result" + ".csv"
-EffectList  = []
-Headers     = []
+# %matplotlib inline
 
-# data = pd.read_csv(CSV_Path)
-# print(data)
+plt.style.use("ggplot")
+# # 虚拟数据
+# x = ["A", "B", "C", "D", "E", "F", "G", "H"]
+# y = [150, 85.2, 65.2, 85, 45, 120, 51, 64]
 
-with open(CSV_Path, encoding='UTF-8')as f:
-    f_csv = csv.reader(f)
-    for row in f_csv:
-        if(len(Headers) == 0):
-            Headers.append(row[1])
-            Headers.append(row[4])
-            Headers.append(row[12])
-        else:
-            EffectList.append([row[1],row[4],row[12]])
+# 柱状图和散点图不同，散点图的(x,y)均为数值变量
+# 柱状图的x轴显示分类变量，有两种处理方式
+# 方式1：自己创建x轴坐标，并提供对应的标签
+# 方式2：让Matplotlib自动完成映射
+
+# 方式1
+# xticks = np.arange(len(x))  # 每根柱子的x轴坐标
+# xlabels = x  # 每根柱子的标签
+# fig, ax = plt.subplots(figsize=(10, 7))
+# ax.bar(x=xticks, height=y, tick_label=xlabels)
+
+# 方式2（推荐）
+# fig, ax = plt.subplots(figsize=(10, 7))
+# ax.bar(x=x, height=y)
+# ax.set_title("Simple Bar Plot", fontsize=15)
+
+# 设置常用参数
+# fig, ax = plt.subplots(figsize=(10, 7))
+# ax.bar(
+#     x=x,  # Matplotlib自动将非数值变量转化为x轴坐标
+#     height=y,  # 柱子高度，y轴坐标
+#     width=0.6,  # 柱子宽度，默认0.8，两根柱子中心的距离默认为1.0
+#     align="center",  # 柱子的对齐方式，'center' or 'edge'
+#     color="grey",  # 柱子颜色
+#     edgecolor="red",  # 柱子边框的颜色
+#     linewidth=2.0  # 柱子边框线的大小
+# )
+# ax.set_title("Adjust Styles of Bar plot", fontsize=15)
+
+# 一个常见的场景是：每根柱子上方添加数值标签
+# 步骤：
+# 1. 准备要添加的标签和坐标
+# 2. 调用ax.annotate()将文本添加到图表
+# 3. 调整样式，例如标签大小，颜色和对齐方式
+# xticks = ax.get_xticks()
+# for i in range(len(y)):
+#     xy = (xticks[i], y[i] * 1.03)
+#     s = str(y[i])
+#     ax.annotate(
+#         text=s,  # 要添加的文本
+#         xy=xy,  # 将文本添加到哪个位置
+#         fontsize=12,  # 标签大小
+#         color="blue",  # 标签颜色
+#         ha="center",  # 水平对齐
+#         va="baseline"  # 垂直对齐
+#     )
 
 
-print(Headers)
-print(EffectList)
+#data
+shops = ["A", "B", "C", "D", "E", "F"]
+sales_product_1 = [100, 85, 56, 42, 72, 15]
+sales_product_2 = [50, 120, 65, 85, 25, 55]
+sales_product_3 = [20, 35, 45, 27, 55, 65]
 
-with open(Result_Path,'w', encoding='UTF-8-sig')as f:
-    f_csv = csv.writer(f)
-    f_csv.writerow(Headers)
-    f_csv.writerows(EffectList)
+# 创建分组柱状图，需要自己控制x轴坐标
+xticks = np.arange(len(shops))
+
+fig, ax = plt.subplots(figsize=(10, 7))
+# 所有门店第一种产品的销量，注意控制柱子的宽度，这里选择0.25
+ax.bar(xticks, sales_product_1, width=0.25, label="Product_1", color="red")
+# 所有门店第二种产品的销量，通过微调x轴坐标来调整新增柱子的位置
+ax.bar(xticks + 0.25, sales_product_2, width=0.25, label="Product_2", color="blue")
+# 所有门店第三种产品的销量，继续微调x轴坐标调整新增柱子的位置
+ax.bar(xticks + 0.5, sales_product_3, width=0.25, label="Product_3", color="green")
+
+ax.set_title("Grouped Bar plot", fontsize=15)
+ax.set_xlabel("Shops")
+ax.set_ylabel("Product Sales")
+ax.legend()
+
+# 最后调整x轴标签的位置
+ax.set_xticks(xticks + 0.25)
+ax.set_xticklabels(shops)
+
+
+plt.show()
